2d11795bbcf20f17a68a3196e92a85037d9f8cb5
Dylan Crawshaw <dcrawsh@gmail.com>
2019-08-26 01:58:01 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -3,6 +3,12 @@
 #   "Sarah" => 6, 
 #   "Amanda" => 27
 # }
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
+
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
